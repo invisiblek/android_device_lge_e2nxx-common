6e9ed9f8b77b29db3b5d0d79cf6202a0366480a2
Dan Pasanen <dan.pasanen@gmail.com>
2015-03-19 10:35:58 -0500

    sepolicy: initial selinux configuration
    Change-Id: I91ba5098575739fd9cbf89cf9361c20f32c2e406

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -101,9 +101,16 @@ TARGET_USERIMAGES_USE_EXT4 := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-# TODO
-# BOARD_SEPOLICY_DIRS += device/lge/e2nxx-common/sepolicy
-# BOARD_SEPOLICY_UNION += \
+BOARD_SEPOLICY_DIRS += device/lge/e2nxx-common/sepolicy
+BOARD_SEPOLICY_UNION += \
+  file_contexts \
+  file.te \
+  init_shell.te \
+  mpdecision.te \
+  rmt_storage.te \
+  sensors.te \
+  thermal-engine.te \
+  vold.te
 
 # Time services
 # TODO (needs libtime_genoff)
