8f7cf21f7d8535c0c77aeb3f4ad098014b6abbb1
c457 <android.c357@gmail.com>
2015-03-19 20:52:15 -0500

    e2nxx: always bump images
    * Although ms395 has unlocked BL we should sign for other variants
    instead of doing it per device as it has no affect on ms395.

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -17,9 +17,7 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTAL
 	$(call pretty,"Target boot image: $@")
 	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
-ifeq ($(TARGET_REQUIRES_BUMP),true)
 	$(hide) $(BUMP) $@ $@
-endif
 	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
 
 ## Overload recoveryimg generation: Same as the original, + --dt arg
@@ -29,7 +27,5 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) \
 	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
-ifeq ($(TARGET_REQUIRES_BUMP),true)
 	$(hide) $(BUMP) $@ $@
-endif
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
